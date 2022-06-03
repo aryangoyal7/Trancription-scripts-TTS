d9d97bd7a848e7fdbaca2392dd227eea4d79cf3a
aniketp02 <pokleaniket92@gmail.com>
2022-06-03 16:59:08 +0530

    Split data into train, test and validation set.

--- data_split.py
+++ data_split.py
@@ -3,12 +3,12 @@ import numpy as np
 import random
 np.random.seed(2)
 
-maxLen = 4000 #length of the dataset
+maxLen = 12000 #length of the dataset
 data = range(1, maxLen)
 # random.shuffle(data)
 
-testSize = maxLen * 0.1 # 10percent data as test set
-validSize = maxLen * 0.05 # 5percent data as validation set
+testSize = int(maxLen * 0.1) # 10percent data as test set
+validSize = int(maxLen * 0.05) # 5percent data as validation set
 test_ = random.sample(data, testSize)
 valid_ = random.sample(data, validSize)
 
@@ -17,7 +17,7 @@ valid_nos = np.sort(valid_)
 
 print(test_nos)
 print(valid_nos)
-f = open("train data path", 'r')
+f = open("all_data.txt", 'r')
 f1 = open('test.txt', 'a')
 f2 = open('valid.txt', 'a')
 f3 = open('train_new.txt', 'a') #stores all the data not in valid.txt or test.txt
@@ -27,10 +27,10 @@ v = 0
 i = 0
 
 for line in f:
-    if(i == test_nos[t] and t < testSize):
+    if(t < testSize and i == test_nos[t]):
         f1.write(line)
         t += 1
-    if(i == valid_nos[v] and v < validSize):
+    if(v < validSize and i == valid_nos[v]):
         f2.write(line)
         v += 1
     else:
